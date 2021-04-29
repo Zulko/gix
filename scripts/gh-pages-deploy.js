0f7f676397df0baacb26e039a88220d57016c121
zulko <valentin.zulkower@gmail.com>
2021-04-28 21:19:57 -0400

    fixing the deploy

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -11,6 +11,7 @@ const fs = require('fs');
     // await execa("yarn", ["build"]);
     // Understand if it's dist or build folder
     const folderName = fs.existsSync('dist') ? 'dist' : 'build';
+    await execa('touch', [`${folderName}/.nojekyll`]);
     await execa('git', ['--work-tree', folderName, 'add', '--all']);
     await execa('git', ['--work-tree', folderName, 'commit', '-m', 'gh-pages']);
     console.log('Pushing to gh-pages...');

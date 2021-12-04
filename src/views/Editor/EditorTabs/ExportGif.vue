9a1672805576ea147d310027963d08a89d49af7e
zulko <valentin.zulkower@gmail.com>
2021-12-04 15:36:14 -0500

    added MP4 export

--- src/views/Editor/EditorTabs/ExportGif.vue
+++ src/views/Editor/EditorTabs/ExportGif.vue
@@ -2,7 +2,13 @@
 .export-gif
   b-field(group-multiline, grouped, style="margin-top: 2em")
     p.control
-      b-field(label="GIF speed", label-position="on-border")
+      b-field(label="Format", label-position="on-border")
+        b-select(v-model="exportFormat")
+          option(value="GIF", label="GIF because I'm a purist")
+          option(value="mp4", label="MP4 because let's face it")
+
+    p.control
+      b-field(label="Speed", label-position="on-border")
         b-select(v-model="gifParams.speedFactor")
           option(
             v-for="(value, i) in [0.1, 0.2, 0.5, 0.8, 1, 1.2, 1.5, 2, 3, 5]",
@@ -11,7 +17,7 @@
             :label="`${parseInt(100 * value)}` + '%'"
           )
     p.control
-      b-field(label="GIF scale", label-position="on-border")
+      b-field(label="Scale", label-position="on-border")
         b-select(v-model="gifParams.scale")
           option(
             v-for="(value, i) in [0.1, 0.2, 0.5, 0.8, 1, 1.2, 1.5, 2, 3, 5]",
@@ -20,7 +26,7 @@
             :label="`${parseInt(100 * value)}` + '%'"
           )
     p.control
-      b-field(label="GIF FPS", label-position="on-border")
+      b-field(label="FPS", label-position="on-border")
         b-select(v-model="gifParams.useDefaultFps")
           option(
             :value="true",
@@ -33,13 +39,13 @@
           v-model="gifParams.fps",
           controls-position="compact"
         )
-    p.control
+    p.control(v-if="exportFormat === 'GIF'")
       b-field(label="Transparency", label-position="on-border")
         b-select(v-model="gifParams.transparency")
           option(value="None", label="No transparency")
           option(value="#00FF00", label="Green background")
           option(value="#000000", label="Black background")
-    p.control
+    p.control(v-if="exportFormat === 'GIF'")
       b-field(label="Dithering", label-position="on-border")
         b-select(v-model="gifParams.dithering")
           option(
@@ -51,7 +57,8 @@
           option(value="floyd", label="Custom FPS")
   div(style="text-align: center; margin-top: 2em")
     div(v-if="!gifGeneration.inProgress")
-      b-button.generate-gif-button(@click="generateGif", type="is-primary") Generate the GIF
+      b-button.generate-gif-button(@click="generateGif", type="is-primary").
+        Generate the {{ exportFormat }}
     div(v-else)
       b-field.gif-export-progress-bars
         b-progress(
@@ -61,27 +68,35 @@
           :value="progressBarValue",
           :type="progressBarValue === 100 ? 'is-success' : 'is-info'"
         ) Frames rendering
-      b-field.gif-export-progress-bars
+      b-field.gif-export-progress-bars(v-if="exportFormat === 'GIF'")
         b-progress(
           format="percent",
           :max="100",
           show-value,
           :value="100 * (gifGeneration.progress.gif || 0)",
           :type="gifGeneration.progress.gif === 1 ? 'is-success' : 'is-info'"
-        ) GIF creation
+        ) {{ exportFormat }} creation
 
-    .result(v-if="gifDataURL && !gifGeneration.inProgress")
-      .rendered-gif
-        img(:src="gifDataURL")
-        b-field(grouped, position="is-centered")
-          a(:href="gifDataURL", :download="$store.state.project.infos.title")
-            b-button(icon-left="download").
-              Download ({{(0.75*gifDataURL.length / 1000000).toFixed(2)}}Mo)
+    .result(v-if="!gifGeneration.inProgress")
+      div(v-if="exportFormat === 'GIF' && gifDataURL")
+        .rendered-gif
+          img(:src="gifDataURL")
+          b-field(grouped, position="is-centered")
+            a(:href="gifDataURL", :download="$store.state.project.infos.title")
+              b-button(icon-left="download").
+                Download ({{(0.75*gifDataURL.length / 1000000).toFixed(2)}}Mo)
+      div(v-if="exportFormat === 'mp4' && mp4DataURL")
+        .rendered-gif
+          video(:src="mp4DataURL", autoplay=1, loop=1)
+          b-field(grouped, position="is-centered")
+            a(:href="mp4DataURL", :download="$store.state.project.infos.title")
+              b-button(icon-left="download").
+                Download ({{(0.75*mp4DataURL.length / 1000).toFixed(0)}}Ko)
 </template>
 <script>
 import { mapMutations } from 'vuex';
 import GixPlayer from '../../../components/GixAnimation/GixPlayer.vue';
-import { renderGixToGif } from '../../../gix-renderer/index';
+import { renderGixToGif, renderGixToMp4 } from '../../../gix-renderer/index';
 
 export default {
   props: {
@@ -101,7 +116,9 @@ export default {
         transparency: 'None',
         dithering: 'None',
       },
+      exportFormat: 'GIF',
       gifDataURL: null,
+      mp4DataURL: null,
       ditherings: ['None', 'FloydSteinberg', 'FalseFloydSteinberg', 'Stucki', 'Atkinson'],
     };
   },
@@ -110,15 +127,28 @@ export default {
     async generateGif() {
       this.gifGeneration = { inProgress: true, progress: { gif: 0, currentFrame: 1 } };
       this.setFreezeGifPreview(true);
-      this.gifDataURL = await renderGixToGif(
-        JSON.parse(JSON.stringify(this.$store.state.project)),
-        {
-          ...this.gifParams,
-          fps: this.useDefaultFps ? this.$store.state.project.fps : this.gifParams.fps,
-          frameServers: window.frameServers,
-        },
-        this.updateProgress,
-      );
+      if (this.exportFormat === 'GIF') {
+        this.gifDataURL = await renderGixToGif(
+          JSON.parse(JSON.stringify(this.$store.state.project)),
+          {
+            ...this.gifParams,
+            fps: this.useDefaultFps ? this.$store.state.project.fps : this.gifParams.fps,
+            frameServers: window.frameServers,
+          },
+          this.updateProgress,
+        );
+      } else if (this.exportFormat === 'mp4') {
+        this.mp4DataURL = await renderGixToMp4(
+          JSON.parse(JSON.stringify(this.$store.state.project)),
+          {
+            ...this.gifParams,
+            fps: this.useDefaultFps ? this.$store.state.project.fps : this.gifParams.fps,
+            frameServers: window.frameServers,
+          },
+          this.updateProgress,
+        );
+      }
+
       this.setFreezeGifPreview(false);
       this.gifGeneration = { ...this.gifGeneration, inProgress: false };
     },

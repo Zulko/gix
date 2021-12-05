3a0eed8fca02b85cee2ef99fbafa55a6bb12ead8
username <email>
2021-12-05 23:52:47 +0000

    gh-pages

---
+++ precache-manifest.816c55e2480fe015585ef4763ba7aefa.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8f3eb632cfe9d25a0c1",
+    "url": "/gix/css/app.39af884f.css"
+  },
+  {
+    "revision": "22935157c3a86e7ce67d",
+    "url": "/gix/css/chunk-vendors.ca7f275c.css"
+  },
+  {
+    "revision": "1326a1901973d33221c4",
+    "url": "/gix/css/docs.0b34e3ea.css"
+  },
+  {
+    "revision": "5972208f258279a35e7f",
+    "url": "/gix/css/editor.bd325403.css"
+  },
+  {
+    "revision": "9d77e745526b2f05d7e5",
+    "url": "/gix/css/editor~home.d4821114.css"
+  },
+  {
+    "revision": "d51e95aff5457c301051",
+    "url": "/gix/css/home.9615a705.css"
+  },
+  {
+    "revision": "e5a25422b457b3ea720d6581eab2375a",
+    "url": "/gix/favicon.png"
+  },
+  {
+    "revision": "1a0c81e49a8e7ccec0b18f5f92b9e6b9",
+    "url": "/gix/gif.worker.js"
+  },
+  {
+    "revision": "ebf8d01e64ea4cc7eda0e4c7ddca68b5",
+    "url": "/gix/img/background-2.svg"
+  },
+  {
+    "revision": "21a5af2955b259ba7b748a51f69470a3",
+    "url": "/gix/img/background.svg"
+  },
+  {
+    "revision": "c5daef8c32852b07b82a345b5d3114c5",
+    "url": "/gix/index.html"
+  },
+  {
+    "revision": "45b156c33b466fce9590",
+    "url": "/gix/js/HME.e620aa22.js"
+  },
+  {
+    "revision": "d8f3eb632cfe9d25a0c1",
+    "url": "/gix/js/app.b2631751.js"
+  },
+  {
+    "revision": "22935157c3a86e7ce67d",
+    "url": "/gix/js/chunk-vendors.1d68ea8c.js"
+  },
+  {
+    "revision": "1326a1901973d33221c4",
+    "url": "/gix/js/docs.94e228bd.js"
+  },
+  {
+    "revision": "5972208f258279a35e7f",
+    "url": "/gix/js/editor.7adf1682.js"
+  },
+  {
+    "revision": "9d77e745526b2f05d7e5",
+    "url": "/gix/js/editor~home.5fcc570e.js"
+  },
+  {
+    "revision": "d51e95aff5457c301051",
+    "url": "/gix/js/home.0b7678ca.js"
+  },
+  {
+    "revision": "c1608d9c1fb0add78f2edafcfb67fade",
+    "url": "/gix/manifest.json"
+  },
+  {
+    "revision": "cf964ea94c48d3f7919febfcacc5c2fd",
+    "url": "/gix/media-url2.worker.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/gix/robots.txt"
+  }
+]);
